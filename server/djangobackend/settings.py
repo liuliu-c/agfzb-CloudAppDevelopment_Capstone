6a47ac130f6ad22ef9c621878a0e7079f4f75d99
liuliu-c <liuliu7960@gmail.com>
2021-08-18 05:07:35 +0000

    Adding temporary changes to Github

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["liuliu123.us-south.cf.appdomain.cloud"]
+ALLOWED_HOSTS = ['localhost']#["liuliu123.us-south.cf.appdomain.cloud"]
 
 
 # Application definition
